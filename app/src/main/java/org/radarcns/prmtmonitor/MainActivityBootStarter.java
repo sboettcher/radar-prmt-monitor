1ae99c5f399c384ba8aa9714f1ee3b8131f7feae
Sebastian Boettcher <seb.boett@gmail.com>
2018-05-30 18:48:30 +0200

    initial commit, copy from RADAR-base/radar-prmt-android/tree/ff1d8a4; renamed package, some classes

---
+++ app/src/main/java/org/radarcns/prmtmonitor/MainActivityBootStarter.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 The Hyve
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarcns.prmtmonitor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Objects;
+
+/**
+ * Starts MainActivity on boot if configured to do so
+ */
+public class MainActivityBootStarter extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, MonitorMainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
---
+++ app/src/main/java/org/radarcns/prmtmonitor/MonitorMainActivityView.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2017 The Hyve
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.radarcns.prmtmonitor;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.radarcns.android.MainActivityView;
+import org.radarcns.android.RadarConfiguration;
+import org.radarcns.android.device.DeviceServiceProvider;
+import org.radarcns.data.TimedInt;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import static org.radarcns.android.RadarConfiguration.CONDENSED_DISPLAY_KEY;
+
+public class MonitorMainActivityView implements Runnable, MainActivityView {
+    private static final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.US);
+
+    private final MonitorMainActivity mainActivity;
+    private final List<DeviceRowView> rows = new ArrayList<>();
+    private List<DeviceServiceProvider> savedConnections;
+
+    private long previousTimestamp;
+    private volatile String newServerStatus;
+
+    // View elements
+    private TextView mServerMessage;
+    private TextView mUserId;
+    private String userId;
+    private String previousUserId;
+    private TextView mProjectId;
+    private String projectId;
+    private String previousProjectId;
+
+    MonitorMainActivityView(MonitorMainActivity activity) {
+        this.mainActivity = activity;
+        this.previousUserId = "";
+
+        initializeViews();
+
+        createRows();
+    }
+
+    private void createRows() {
+        if (mainActivity.getRadarService() != null
+                && !mainActivity.getRadarService().getConnections().equals(savedConnections)) {
+            ViewGroup root = mainActivity.findViewById(R.id.deviceTable);
+            while (root.getChildCount() > 1) {
+                root.removeView(root.getChildAt(1));
+            }
+            rows.clear();
+            boolean condensed = RadarConfiguration.getInstance().getBoolean(CONDENSED_DISPLAY_KEY, true);
+            for (DeviceServiceProvider provider : mainActivity.getRadarService().getConnections()) {
+                if (provider.isDisplayable()) {
+                    rows.add(new DeviceRowView(mainActivity, provider, root, condensed));
+                }
+            }
+            savedConnections = mainActivity.getRadarService().getConnections();
+        }
+    }
+
+    public void update() {
+        createRows();
+
+        userId = mainActivity.getUserId();
+        projectId = mainActivity.getProjectId();
+        for (DeviceRowView row : rows) {
+            row.update();
+        }
+        if (mainActivity.getRadarService() != null) {
+            newServerStatus = getServerStatusMessage();
+        }
+        mainActivity.runOnUiThread(this);
+    }
+
+    private String getServerStatusMessage() {
+        TimedInt numberOfRecords = mainActivity.getRadarService().getLatestNumberOfRecordsSent();
+
+        String message = null;
+        if (numberOfRecords != null && numberOfRecords.getTime() >= 0 && previousTimestamp != numberOfRecords.getTime()) {
+            previousTimestamp = numberOfRecords.getTime();
+
+            String messageTimeStamp = timeFormat.format(numberOfRecords.getTime());
+
+            if (numberOfRecords.getValue() < 0) {
+                message = String.format(Locale.US, "last upload failed at %1$s", messageTimeStamp);
+            } else {
+                message = String.format(Locale.US, "last upload at %1$s", messageTimeStamp);
+            }
+        }
+        return message;
+    }
+
+    private void initializeViews() {
+        mainActivity.setContentView(R.layout.compact_overview);
+
+        mServerMessage = mainActivity.findViewById(R.id.statusServerMessage);
+
+        mUserId = mainActivity.findViewById(R.id.inputUserId);
+        mProjectId = mainActivity.findViewById(R.id.inputProjectId);
+    }
+
+    @Override
+    public void run() {
+        for (DeviceRowView row : rows) {
+            row.display();
+        }
+        updateServerStatus();
+        setUserId();
+    }
+
+    private void updateServerStatus() {
+        String message = newServerStatus;
+
+        if (message != null) {
+            mServerMessage.setText(message);
+        }
+    }
+
+    private void setUserId() {
+        if (!Objects.equals(userId, previousUserId)) {
+            if (userId == null) {
+                mUserId.setVisibility(View.GONE);
+            } else {
+                if (previousUserId == null) {
+                    mUserId.setVisibility(View.VISIBLE);
+                }
+                mUserId.setText(mainActivity.getString(R.string.user_id_message, userId));
+            }
+            previousUserId = userId;
+        }
+        if (!Objects.equals(projectId, previousProjectId)) {
+            if (projectId == null) {
+                mProjectId.setVisibility(View.GONE);
+            } else {
+                if (previousProjectId == null) {
+                    mProjectId.setVisibility(View.VISIBLE);
+                }
+                mProjectId.setText(mainActivity.getString(R.string.study_id_message, projectId));
+            }
+            previousProjectId = projectId;
+        }
+    }
+}
